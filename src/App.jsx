4a6f95f3023294b1bb78a44d255b7a1789d5e3d4
thiagoferreirapy <tf938383@gmail.com>
2024-10-23 12:12:54 -0300

    Modificando rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Header } from "./layouts/Header/Header";
-import { Footer } from "./layouts/Footer/Footer";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./Routes/AppRoutes";
 import MainLayout from "./Layouts/MianLayout";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./Providers/ThemeProvider";
+import { Header } from "./Layouts/Header/Header";
+import { Footer } from "./Layouts/Footer/Footer";
 
 function App() {
   return (
